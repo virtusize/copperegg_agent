b570da4df161f779557f4236b1ad1842c729a503
Igor Khomyakov <igor@virtusize.com>
2014-09-17 14:19:35 +0400

    Feature: enhanced logging Bugfix: parser greenlet crush fixed

--- nginx_agent.py
+++ nginx_agent.py
@@ -30,14 +30,14 @@ gevent.monkey.patch_all
 
 import gevent
 from gevent.queue import Queue
-
+import logging
 import signal
 import time
 import json
 import sys
 import requests
 
-POST_URL = 'http://api.copperegg.com/v2/revealmetrics/samples/%s.json'
+POST_URL = 'http://api.copperegg.com/v2/revealmetrics/samples/{}.json'
 
 DRY_RUN = False
 VERBOSE = False
@@ -45,8 +45,7 @@ HTTP_VERBS = ['HEAD', 'PUT', 'GET', 'POST']
 
 
 def verbose(msg):
-    if VERBOSE:
-        print(msg)
+    logging.debug(msg)
 
 
 def get_metrics_nginx_backend(queue):
@@ -56,44 +55,51 @@ def get_metrics_nginx_backend(queue):
         try:
             backend, request, status, response = line.rstrip().split('|', 3)
             if status != '200':
-                verbose("Skipped %s %s" % (status, request))
+                verbose("Skipped {} {}".format(status, request))
                 continue
             host, port = backend.split(':', 1)
             verb, url, http = request.split(' ', 2)
         except (TypeError, ValueError):
-            verbose("Parser error %s" % line)
+            verbose("Parser error {}".format(line))
             continue
         if url in skip_list:
-            verbose("Skipped: %s" % url)
+            verbose("Skipped: {}".format(url))
             continue
         if verb not in HTTP_VERBS:
-            verbose("Unknown http verb %s" % request)
+            verbose("Unknown http verb {}".format(request))
             continue
-        metrics = defaultdict(dict)
-        metrics['identifier'] = "backend_%s_%s" % (port, verb.lower())
-        metrics['values']['response_time'] = response
-        if VERBOSE:
-            count += 1
-            if count >= 100:
-                print("Producer %s queue size: %s" % (verb, queue[verb].qsize()))
-                count = 0
-        try:
-            count += 1
-            queue[verb].put_nowait(metrics)
-        except gevent.queue.Full as e:
-            print("%s queue congestion. Size: %s. Discarding data." %
-                  (verb, queue[verb].qsize()))
-            gevent.sleep(0.5)
-        finally:
-            gevent.sleep(0.1)
 
-    for verb in HTTP_VERBS:
-        queue[verb].put_nowait(None)  # put a poison pill
-        verbose("Put poison pill for consumer")
+        # 127.0.0.1:11004, 127.0.0.1:11003|GET /api/vs-widget/v2
+        #       HTTP/1.1|200|1.492, 0.020
+        for back, rtime in zip(backend.split(', '), response.split(' ,')):
+            host, port = back.split(':', 1)
+            metrics = defaultdict(lambda: defaultdict(int))
+            metrics['identifier'] = "backend_{}_{}".format(port, verb.lower())
+            metrics['values']['response_time'] = rtime
+
+            if VERBOSE:
+                count += 1
+                if count >= 100:
+                    verbose("Producer {} queue size: {}".format(
+                        verb,
+                        queue[verb].qsize()))
+                    count = 0
+            try:
+                queue[verb].put_nowait(metrics)
+            except gevent.queue.Full:
+                verbose("{} queue congestion."
+                        "Size: {}."
+                        "Discarding data.".format(verb, queue[verb].qsize()))
+
+        gevent.sleep(0)  # end of `for`
+    else:  # got EOF
+        for verb in HTTP_VERBS:
+            queue[verb].put_nowait(None)  # put a poison pill
+            verbose("Put poison pill for consumer")
 
 
 def median(list):
-    verbose("List size %s" % len(list))
+    verbose("List size {}".format(len(list)))
     sorts = sorted(list)
     length = len(sorts)
     if not length % 2:
@@ -103,7 +109,7 @@ def median(list):
 
 def mean(list):
     if len(list) > 0:
-        return float(sum(list)/len(list))
+        return float(sum(list) / len(list))
     else:
         return None
 
@@ -112,18 +118,18 @@ def post_http(api_key, url, metrics):
     headers = {'content-type': 'application/json'}
 
     if DRY_RUN:
-        print("OK: %s" % (json.dumps(metrics)))
+        print("OK: {}".format(json.dumps(metrics)))
     else:
-        verbose("URL: %s" % (url))
+        verbose("URL: {}".format(url))
         try:
             p = requests.post(url,
                               auth=(api_key, 'U'),
                               data=json.dumps(metrics),
                               headers=headers)
             p.raise_for_status()
-            verbose("OK: %s" % (json.dumps(metrics)))
+            verbose("OK: {}".format(json.dumps(metrics)))
         except requests.exceptions.RequestException as e:
-            print("ERR: %s DATA: %s" % (e, json.dumps(metrics)))
+            print("ERR: {} DATA: {}".format(e, json.dumps(metrics)))
 
 
 def post_metrics(queue, s_time, api_key, url):
@@ -131,15 +137,21 @@ def post_metrics(queue, s_time, api_key, url):
         data = {}
         buffer = []
         data = queue.get()
+
         if data is not None:  # it not a poison pill
-            metrics = defaultdict(dict)
-            metrics['identifier'] = data['identifier']
-            buffer.append(float(data['values']['response_time']))
+            try:
+                buffer.append(float(data['values']['response_time']))
+            except ValueError:
+                pass
             while not queue.empty():
                 data = queue.get()
                 if data is not None:
-                    buffer.append(float(data['values']['response_time']))
-            verbose("Consumer queue size %s" % queue.qsize())
+                    try:
+                        buffer.append(float(data['values']['response_time']))
+                    except ValueError:
+                        pass
+            metrics = defaultdict(lambda: defaultdict(int))
+            metrics['identifier'] = data['identifier']
             metrics['timestamp'] = int(time.time())
             metrics['values']['response_time_median'] = median(buffer)
             metrics['values']['response_time_mean'] = mean(buffer)
@@ -147,7 +159,7 @@ def post_metrics(queue, s_time, api_key, url):
             metrics['values']['response_time_max'] = max(buffer)
             post_http(api_key, url, metrics)
         elif buffer:  # got a poison pill, push out collected data
-            metrics = defaultdict(dict)
+            metrics = defaultdict(lambda: defaultdict(int))
             metrics['identifier'] = data['identifier']
             metrics['timestamp'] = int(time.time())
             metrics['values']['response_time_median'] = median(buffer)
@@ -193,6 +205,8 @@ def main():
     if arguments.get('--verbose'):
         print('VERBOSE mode.')
         VERBOSE = True
+        logging.basicConfig(level=logging.DEBUG,
+                            format=u'[%(levelname)s] %(message)s',)
 
     jobs = []
 
@@ -202,7 +216,7 @@ def main():
     queue['HEAD'] = Queue()
     queue['PUT'] = Queue()
 
-    metrics_group = "%s_%s_backend" % (prefix, host)
+    metrics_group = "{}_{}_backend".format(prefix, host)
     jobs.append(gevent.spawn(get_metrics_nginx_backend, queue))
 
     for verb in HTTP_VERBS:
@@ -210,7 +224,7 @@ def main():
                                  queue[verb],
                                  5,  # report every 5 seconds
                                  api_key,
-                                 POST_URL % (metrics_group)))
+                                 POST_URL.format(metrics_group)))
 
     try:
         gevent.joinall(jobs)
